677e2b8ab6d4b127d784a9a0b55b6dc026b92db5
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-06-30 11:17:40 -0500

    UNA PRUEBA DE COMMIT

---
+++ SistemaVenta.DAL/Repositorios/VentaRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.DAL.Repositorios
+
+{
+    public class VentaRepository: GenericRepository<Venta>,IVentaRepository
+    {
+        private readonly DbventangularContext _dbcontext;
+
+        public VentaRepository(DbventangularContext dbcontext):base(dbcontext) 
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<Venta> Registrar(Venta modelo)
+        {
+            Venta ventaGenerada = new Venta();
+
+            using(var transaction = _dbcontext.Database.BeginTransaction())
+            {
+                try 
+                {
+                    foreach (DetalleVenta dv in modelo.DetalleVenta)
+                    {
+                        Producto producto_encontrado = _dbcontext.Productos.Where(p=>p.IdProducto == dv.IdProducto).First();
+                        producto_encontrado.Stock = producto_encontrado.Stock - dv.Cantidad;
+                        _dbcontext.Productos.Update(producto_encontrado);
+
+                    }
+                    await _dbcontext.SaveChangesAsync();
+
+                    NumeroDocumento correlativo = _dbcontext.NumeroDocumentos.First();
+
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaRegistro = DateTime.Now;
+
+                    _dbcontext.NumeroDocumentos.Update(correlativo);
+                    await _dbcontext.SaveChangesAsync();
+
+                    //0001 para generar este formato
+                    int cantidadDigitos = 4;
+                    string ceros = string.Concat(Enumerable.Repeat("0", cantidadDigitos));
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - cantidadDigitos, cantidadDigitos);
+                    modelo.NumeroDocumento = numeroVenta;
+
+                    await _dbcontext.Venta.AddAsync(modelo);
+                    await _dbcontext.SaveChangesAsync();
+
+                    ventaGenerada = modelo;
+                    transaction.Commit();
+
+                } 
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+
+                return ventaGenerada;
+            }
+        }
+    }
+}
