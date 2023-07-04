5abbefd2691cb38825531283947a87884c7ab8b0
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-07-04 16:39:01 -0500

    AutoMapperProfile.cs: se agregaran los mapeos se hacen : origen destino es decir de Rol a RolDTO pero si lo quiero de RolDTO a Rol le coloco ReverseMap así: CreateMap<Rol,RolDTO>().ReverseMap();

---
+++ SistemaVenta.Utility/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+namespace SistemaVenta.Utility
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            #region Rol
+            CreateMap<Rol,RolDTO>().ReverseMap();
+            #endregion Rol
+        
+        }
+    }
+}
