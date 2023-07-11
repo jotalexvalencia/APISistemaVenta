3521dc597e2ab971b83ab5d26d03bfe95552bee6
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-07-11 17:04:48 -0500

    SistemaVenta.BLL\Servicios
    carpeta Contrato: se crean todas las interfaces
    
    se crean igualmente todas las clases que las implementan
    
    SistemaVenta.DTO
    
    DasBoardDTO.cs: se adiciona la prop TotalProductos
    
    SistemaVenta.IOC
    
    Dependencia.cs: se adicionantodos los servicios

--- SistemaVenta.DTO/DashBoardDTO.cs
+++ SistemaVenta.DTO/DashBoardDTO.cs
@@ -10,6 +10,8 @@ namespace SistemaVenta.DTO
     {
         public int TotalVentas { get; set; }
         public string? TotalIngresos { get; set; }
+
+        public int TotalProductos { get; set; }
         public List<VentasSemanaDTO> VentasUltimaSemana { get; set; }
     }
 }
