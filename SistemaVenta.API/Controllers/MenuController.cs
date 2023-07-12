e29280952b4785c3c8950447deeec3e9ea6ee6dc
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-07-12 10:02:24 -0500

    SistemaVenta.API: se elimina el controlador y modelo WeatherForecast el cual viene por defecto, se agregan los controladores para categoria, dashboard, menu, producto, rol, usuario y venta.
    se agrega la carpeta Utilidad clase Response génerica para el manejo de las respuestas de la api

---
+++ SistemaVenta.API/Controllers/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.API.Utilidad;
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private readonly IMenuService _menuServicio;
+
+        public MenuController(IMenuService menuServicio)
+        {
+            _menuServicio = menuServicio;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista(int idUusuario)
+        {
+            var rsp = new Response<List<MenuDTO>>();
+
+            try
+            {
+                rsp.status = true;
+                rsp.Value = await _menuServicio.Lista(idUusuario);
+            }
+            catch (Exception ex)
+            {
+
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+
+            return Ok(rsp);
+        }
+    }
+}
---
+++ SistemaVenta.API/Utilidad/Response.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Identity.Client;
+
+namespace SistemaVenta.API.Utilidad
+{
+    // como respuesta a todas las solicitudes de nuestras apis
+    public class Response<T>
+    {
+        public bool status {  get; set; } 
+        public T Value { get; set; }
+        public string msg { get; set; }
+    }
+}
