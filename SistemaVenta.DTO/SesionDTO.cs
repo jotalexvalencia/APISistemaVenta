549fb8bd106a78d2a5bed679e74507de750704bd
jotalexvalencia <jorgealexandervalencia@gmail.com>
2026-02-26 11:55:57 -0500

    docs: agregada a SesionDTO.cs

--- SistemaVenta.DTO/SesionDTO.cs
+++ SistemaVenta.DTO/SesionDTO.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace SistemaVenta.DTO
 {
+    /// <summary>
+    /// Represents a data transfer object that encapsulates user session information, including user identifier, full
+    /// name, email address, and role description.
+    /// </summary>
+    /// <remarks>This class is typically used to transfer session-related user details between application
+    /// layers, such as from authentication services to client interfaces. It is intended for scenarios where user
+    /// identity and role information are required to manage access or personalize user experience.</remarks>
     public class SesionDTO
     {
         public int IdUsuario { get; set; }
