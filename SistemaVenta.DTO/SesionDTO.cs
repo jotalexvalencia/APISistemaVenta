f0e2f2e61f4576a47c576b635d7a2e9120754428
jotalexvalencia <jorgealexandervalencia@gmail.com>
2026-02-26 18:45:34 -0500

    files: updates DTOS

--- SistemaVenta.DTO/SesionDTO.cs
+++ SistemaVenta.DTO/SesionDTO.cs
@@ -6,21 +6,14 @@ using System.Threading.Tasks;
 
 namespace SistemaVenta.DTO
 {
-    /// <summary>
-    /// Represents a data transfer object that encapsulates user session information, including user identifier, full
-    /// name, email address, and role description.
-    /// </summary>
-    /// <remarks>This class is typically used to transfer session-related user details between application
-    /// layers, such as from authentication services to client interfaces. It is intended for scenarios where user
-    /// identity and role information are required to manage access or personalize user experience.</remarks>
     public class SesionDTO
     {
         public int IdUsuario { get; set; }
-
         public string? NombreCompleto { get; set; }
-
         public string? Correo { get; set; }
-
         public string? RolDescripcion { get; set; }
+
+        // NUEVO: Aquí es donde el Backend devolverá el Token JWT
+        public string? Token { get; set; }
     }
 }
